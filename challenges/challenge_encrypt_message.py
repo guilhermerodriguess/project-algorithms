95d17caa5549d868e19dbfeee48c8560f767cd10
Guilherme <grsguiga@gmail.com>
2023-06-06 14:28:29 -0300

    Iniciando o projeto algorithms

--- challenges/challenge_encrypt_message.py
+++ challenges/challenge_encrypt_message.py
@@ -1,10 +1,9 @@
 def encrypt_message(message: str, key: int):
-
     if not isinstance(key, int):
         raise TypeError("tipo inválido para key")
 
     if not isinstance(message, str):
-        raise TypeError("tipo inválido para message")
+        raise TypeError("tipo inválido para messages")
 
     if key not in range(1, len(message)):
         return "".join(reversed(message))
