878451e559679e4cc3ab7e11700012d0fb61f687
Guilherme <grsguiga@gmail.com>
2023-06-08 17:45:55 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,41 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as err:
+        encrypt_message("message", "invalid_key")
+    assert str(err.value) == "tipo inválido para key"
+
+    with pytest.raises(TypeError) as err:
+        encrypt_message(123, 2)
+    assert str(err.value) == "tipo inválido para messages"
+
+    encrypted = encrypt_message("message", 10)
+    assert encrypted == "egassem", f"Expected 'egassem', got '{encrypted}'"
+
+    encrypted = encrypt_message("message", 3)
+    assert encrypted == "sem_egas", f"Expected 'sem_egas', got '{encrypted}'"
+
+    encrypted = encrypt_message("message", 4)
+    assert encrypted == "ega_ssem", f"Expected 'ega_ssem', got '{encrypted}'"
+
+    encrypted = encrypt_message("message", 0)
+    assert encrypted == "egassem", f"Expected 'egassem', got '{encrypted}'"
+
+    encrypted = encrypt_message("message", 1)
+    assert encrypted == "m_egasse", f"Expected 'm_egasse', got '{encrypted}'"
+
+    encrypted = encrypt_message("", 2)
+    assert encrypted == "", f"Expected '', got '{encrypted}'"
+
+    encrypted = encrypt_message("", 0)
+    assert encrypted == "", f"Expected '', got '{encrypted}'"
+
+    encrypted = encrypt_message("a", 1)
+    assert encrypted == "a", f"Expected 'a', got '{encrypted}'"
+
+    print("All tests passed!")
+
+
+test_encrypt_message()
