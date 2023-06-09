ab92d909eef709486ada1dcd67a14857f0c071f3
Guilherme <grsguiga@gmail.com>
2023-06-09 14:47:43 -0300

    Requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if not nums or isinstance(nums, str):
+        return False
+
+    nums_set = set()
+    for num in nums:
+        if isinstance(num, str) or num < 1:
+            return False
+        if num in nums_set:
+            return num
+        nums_set.add(num)
+
+    return False
+
     raise NotImplementedError
